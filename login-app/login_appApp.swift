19c43ad60b804220792153eb7d8a2d73f198c718
Guilherme Correa <correacgui@gmail.com>
2023-06-01 09:41:06 -0300

    Initial Commit

---
+++ login-app/login_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  login_appApp.swift
+//  login-app
+//
+//  Created by Guilherme de Carvalho Correa on 01/06/23.
+//
+
+import SwiftUI
+
+@main
+struct login_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
